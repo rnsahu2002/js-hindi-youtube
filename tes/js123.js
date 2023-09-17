cd24353b1ee477f309fef9828444cad412fa37bc
rnsahu2002 <rnsahu2002@users.noreply.github.com>
2023-09-17 04:40:12 +0000

    ada

--- tes/js123.js
+++ tes/js123.js
@@ -5,3 +5,8 @@ console.log(typeof(score))
 let changetonumber= Number(score)
 console.log(typeof(changetonumber))
 console.log(changetonumber)
+
+let score1="dada"
+let changetonumber1= Number(score1)
+console.log(typeof(changetonumber1))
+console.log(changetonumber1)
