68d3e112721c46047a2797d55290f2fd07d3d6fb
rnsahu2002 <rnsahu2002@users.noreply.github.com>
2023-09-17 04:43:33 +0000

    lllll

--- tes/js123.js
+++ tes/js123.js
@@ -10,3 +10,20 @@ let score1="dada"
 let changetonumber1= Number(score1)
 console.log(typeof(changetonumber1))
 console.log(changetonumber1)
+
+let a=true
+let b=false
+let booltonum1= Number(a)
+let booltonum2= Number(b)
+console.log(booltonum1)
+console.log(booltonum2)
+
+let c=1
+let d=0
+let numtobool1= Boolean(c)
+let numtobool2= Boolean(d)
+console.log(numtobool1)
+console.log(numtobool2)
+
+
+
