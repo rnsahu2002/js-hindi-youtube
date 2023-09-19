761417446c3079bc0fd59181e904044c65f3578a
rnsahu2002 <rnsahu2002@users.noreply.github.com>
2023-09-19 05:07:36 +0000

    www

--- datatypes.js
+++ datatypes.js
@@ -1,2 +1,8 @@
 // Primitive:- (String,Boolean,Number,BigInt,Null,Undefined,Symbol )
 // Non-Primitive:- Object (array, functions) also called object references.
+
+let str=new String("Ram Sahu")
+console.log(str)
+console.log(str[0])
+// console.log(str)
+
