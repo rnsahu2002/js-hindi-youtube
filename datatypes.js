22e4d978709b43f245e73c6ab07447d311818ecd
rnsahu2002 <rnsahu2002@users.noreply.github.com>
2023-09-19 04:52:38 +0000

    mm

---
+++ datatypes.js
@@ -0,0 +1,2 @@
+// Primitive:- (String,Boolean,Number,BigInt,Null,Undefined,Symbol )
+// Non-Primitive:- Object (array, functions) also called object references.
--- tes/comparison.js
+++ tes/comparison.js
@@ -1,6 +1,36 @@
-console.log(null==0)
-console.log(null>=0)
-console.log(undefined>0)
-console.log(undefined<0)
-console.log(undefined==0)
-console.log("2">1)
+// console.log(null==0)
+// console.log(null>=0)
+// console.log(undefined>0)
+// console.log(undefined<0)
+// console.log(undefined==0)
+// console.log("2">1)
+
+let a;
+let b=undefined
+let c=null
+const d=Symbol('1234')
+const sym1 = Symbol();
+console.log(sym1)
+console.log(typeof sym1)
+const sym3 = Symbol("123");
+const sym2 = Symbol("123");
+console.log(sym2)
+console.log(typeof sym2)
+
+console.log(sym2==sym1)
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+
+
+console.log(sym3)
+console.log(typeof sym3)
+
+
+
+
+
+
+
