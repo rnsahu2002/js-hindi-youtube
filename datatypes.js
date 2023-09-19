a449238e182312b27bdc5a54cc88ec977ac3324b
rnsahu2002 <rnsahu2002@users.noreply.github.com>
2023-09-19 05:22:36 +0000

    dad

--- datatypes.js
+++ datatypes.js
@@ -1,8 +1,47 @@
 // Primitive:- (String,Boolean,Number,BigInt,Null,Undefined,Symbol )
 // Non-Primitive:- Object (array, functions) also called object references.
 
-let str=new String("Ram Sahu")
-console.log(str)
-console.log(str[0])
-console.log(str.toUpperCase)
+// let str=new String("Ram Sahu")
+// // console.log(str)
+// // console.log(str[0])
+// console.log(str.toUpperCase())
+// console.log(str)
+
+
+
+const score = 400
+// console.log(score);
+
+// const balance = new Number(100.90)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(60));
+
+// const otherNumber = 123.812365
+
+// console.log(otherNumber.toPrecision(7));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// // +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(19.6));
+// console.log(Math.ceil(17.2));
+// console.log(Math.floor(11.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+// const min = 10
+// const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
 
--- tes/datatypes.js
+++
@@ -1,8 +0,0 @@
-// Primitive:- (String,Boolean,Number,BigInt,Null,Undefined,Symbol )
-// Non-Primitive:- Object (array, functions) also called object references.
-
-let str=new String("Ram Sahu")
-console.log(str)
-console.log(str[0])
-// console.log(str)
-
